5b268910dcf2421b452aee6a99b165f202ed1608
Balaji-Krishnamoorthi <balajikrishnamoorthi4@gmail.com>
2026-03-26 11:50:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,48 @@ function showMessage(event) {
     // form clear panna
     document.querySelector("form").reset();
 }
+function showEvents() {
+
+    let category = document.getElementById("category").value;
+    let eventDropdown = document.getElementById("events");
+
+    eventDropdown.innerHTML = "";
+
+    if (category === "tech") {
+
+        let techEvents = [
+            "Paper Presentation",
+            "Project Expo",
+            "Codethon",
+            "Hackathon",
+            "Coding Contest",
+            "Debugging"
+        ];
+
+        techEvents.forEach(event => {
+            let option = document.createElement("option");
+            option.text = event;
+            eventDropdown.add(option);
+        });
+
+    }
+
+    else if (category === "nontech") {
+
+        let nonTechEvents = [
+            "E-Sports",
+            "Treasure Hunt",
+            "IPL Mini Auction",
+            "Dance",
+            "Singing",
+            "Drama"
+        ];
+
+        nonTechEvents.forEach(event => {
+            let option = document.createElement("option");
+            option.text = event;
+            eventDropdown.add(option);
+        });
+
+    }
+}
